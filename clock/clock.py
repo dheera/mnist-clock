2bc01169f4a0fa3a82e7a8818a570d58c7e40a32
dheera <dheera@dheera.net>
2018-08-19 07:01:15 +0000

    update

--- clock/clock.py
+++ clock/clock.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+# <debug>
 import cv2
+# </debug>
+
 import json
 import os
 import random
@@ -10,19 +13,31 @@ import numpy as np
 
 from PIL import Image
 
+def update_display(pos, img):
+    print("updating position %d" % pos)
+
 def update_clock():
     t = time.localtime()
-    time_display = "%02d%02d" % (t.tm_hour, t.tm_min)
-    imgs = []
-    for digit in time_display:
-        img = Image.open(os.path.join(
+    time_string = "%02d%02d" % (t.tm_hour, t.tm_min)
+    if time_string == update_clock.last_time_string:
+        return
+
+    for pos, digit in enumerate(time_string):
+        img_pil = Image.open(os.path.join(
             "digits", digit, random.choice(digit_filenames[digit])
         ))
-        imgs.append(np.reshape(img.getdata(), img.size))
-    imgs = np.concatenate(imgs, axis=1).astype(np.uint8)
-    imgs = cv2.resize(imgs, (imgs.shape[1]*10, imgs.shape[0]*10))
-    cv2.imshow('clock', imgs)
-    cv2.waitKey(1)
+        img = np.reshape(img_pil.getdata(), img_pil.size).astype(np.uint8)
+        update_display(pos, img)
+
+    update_clock.last_time_string = time_string
+update_clock.last_time_string = "----"
+
+# <debug>
+#    imgs = np.concatenate(imgs, axis=1).astype(np.uint8)
+#    imgs = cv2.resize(imgs, (imgs.shape[1]*10, imgs.shape[0]*10))
+#    cv2.imshow('clock', imgs)
+#    cv2.waitKey(1)
+# </debug>
 
 if __name__ == "__main__":
 
